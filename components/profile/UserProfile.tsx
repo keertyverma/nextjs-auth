4a6414886c7469628015a588be3bd8bf57610c73
Keerty Verma <keertyverma1303@gmail.com>
2023-06-22 17:48:45 +0530

    add client-side page guard (route protection)

--- components/profile/UserProfile.tsx
+++ components/profile/UserProfile.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./UserProfile.module.css";
 import ProfileForm from "./ProfileForm";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const UserProfile = () => {
-  // Redirect away if NOT auth
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status !== "authenticated") {
+      router.push("/auth");
+    }
+  }, [status]);
+
+  if (status === "loading") {
+    return <p className={classes.profile}>Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
