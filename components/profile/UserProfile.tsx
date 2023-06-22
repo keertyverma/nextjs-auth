58373bd74b1ffc0c96598adedd50522d3ecc8f92
Keerty Verma <keertyverma1303@gmail.com>
2023-06-22 18:09:39 +0530

    server side page guard

--- components/profile/UserProfile.tsx
+++ components/profile/UserProfile.tsx
@@ -1,23 +1,7 @@
-import React, { useEffect } from "react";
-import classes from "./UserProfile.module.css";
 import ProfileForm from "./ProfileForm";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import classes from "./UserProfile.module.css";
 
 const UserProfile = () => {
-  const { status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status !== "authenticated") {
-      router.push("/auth");
-    }
-  }, [status]);
-
-  if (status === "loading") {
-    return <p className={classes.profile}>Loading...</p>;
-  }
-
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,8 +1,27 @@
+import { GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/react";
+
 import UserProfile from "@/components/profile/UserProfile";
-import React from "react";
 
 const profile = () => {
   return <UserProfile />;
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
 
 export default profile;
